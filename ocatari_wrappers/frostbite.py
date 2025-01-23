27d757ebc2c72768dc86c31c7b46f7ee760e97cb
vanillawhey <cedric.derstroff@gmx.net>
2025-01-23 16:18:14 +0100

    Fix: make DL Wrappers more efficient

--- ocatari_wrappers/frostbite.py
+++ ocatari_wrappers/frostbite.py
@@ -22,29 +22,30 @@ class DLWrapper(MaskedBaseWrapper):
                 if x + w > 0 and y + h > 0:
                     for i in range(max(0, y), min(y + h, 209)):
                         for j in range(max(0, x), min(x + w, 159)):
+                            pixel = tuple(img[i, j, :])
                             # Igloo
-                            if o.category == "House" and list(img[i, j, :]) == [142, 142, 142]:
+                            if o.category == "House" and pixel == (142, 142, 142):
                                 state[7][i, j] = 0
                             # Player
-                            elif list(img[i, j, :]) in [[162, 98, 33], [162, 162, 42], [198, 108, 58], [142, 142, 142]]:
+                            elif pixel in {(162, 98, 33), (162, 162, 42), (198, 108, 58), (142, 142, 142)}:
                                 state[0][i, j] = 0
                             # Bad Animal
-                            elif list(img[i, j, :]) in [[132, 144, 252], [210, 210, 64], [213, 130, 74]]:
+                            elif pixel in {(132, 144, 252), (210, 210, 64), (213, 130, 74)}:
                                 state[1][i, j] = 0
                             # Land
-                            elif list(img[i, j, :]) in [[192, 192, 192], [74, 74, 74]]:
+                            elif pixel in {(192, 192, 192), (74, 74, 74)}:
                                 state[2][i, j] = 0
                             # Bear
-                            elif o.category == "Bear" and list(img[i, j, :]) in [[111, 111, 111], [214, 214, 214]]:
+                            elif o.category == "Bear" and pixel in {(111, 111, 111), (214, 214, 214)}:
                                 state[3][i, j] = 0
                             # Unvisited Floes
-                            elif list(img[i, j, :]) == [214, 214, 214]:
+                            elif pixel == (214, 214, 214):
                                 state[4][i, j] = 0
                             # Visited Flows
-                            elif list(img[i, j, :]) == [84, 138, 210]:
+                            elif pixel == (84, 138, 210):
                                 state[5][i, j] = 0
                             # Good Animal
-                            elif list(img[i, j, :]) == [111, 210, 111]:
+                            elif pixel == (111, 210, 111):
                                 state[6][i, j] = 0
 
         return self.create_obs(state)
@@ -60,6 +61,14 @@ class DLGroupedWrapper(MaskedBaseWrapper):
     def __init__(self, env, buffer_window_size=4):
         super().__init__(env, buffer_window_size, include_pixels=True, num_planes=1)
 
+        self._pixel_set = {
+            (162, 98, 33), (162, 162, 42), (198, 108, 58), (142, 142, 142),  # Player
+            (132, 144, 252), (210, 210, 64), (213, 130, 74),  # Bad Animal
+            (111, 111, 111), (214, 214, 214),  # Bear and Unvisited Floes
+            (84, 138, 210),  # Visited Flows
+            (111, 210, 111),  # Good Animal
+        }
+
     def observation(self, observation):
         state = np.ones((210, 160)) * 255
         img = self.unwrapped.ale.getScreenRGB()  # noqa: super test for ale
@@ -69,13 +78,7 @@ class DLGroupedWrapper(MaskedBaseWrapper):
                 if x + w > 0 and y + h > 0:
                     for i in range(max(0, y), min(y + h, 209)):
                         for j in range(max(0, x), min(x + w, 159)):
-                            if list(img[i, j, :]) in [
-                                [162, 98, 33], [162, 162, 42], [198, 108, 58], [142, 142, 142],  # Player
-                                [132, 144, 252], [210, 210, 64], [213, 130, 74],  # Bad Animal
-                                [111, 111, 111], [214, 214, 214],  # Bear and Unvisited Floes
-                                [84, 138, 210],  # Visited Flows
-                                [111, 210, 111], # Good Animal
-                                         ]:
+                            if tuple(self._img[i, j, :]) in self._pixel_set:
                                 state[i, j] = 0
 
         return self.create_obs([state])
